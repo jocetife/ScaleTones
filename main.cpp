255d594b7cb4e90f0ba832068ad0241eb0b2cb6d
Jocelyn <jocelyn.tirado6677@alumnos.udg.mx>
2026-02-28 02:45:20 -0600

    basic structured logic

--- main.cpp
+++ main.cpp
@@ -8,47 +8,6 @@
 
 using namespace std;
 
-enum class ScaleType{
-    CM, DM, EM, FM, GM, AM, BM,     Cm, Dm, Em, Fm, Gm, Am, Bm,
-    F_SHARP_m, G_FLAT_M, E_FLAT_m, D_SHARP_m, D_FLAT_M, B_FLAT_m, A_FLAT_M, E_FLAT_M, B_FLAT_M,
-
-    UNKNOWN
-};
-class Scale {
-public:
-    ScaleType parseScale (const string& tone){
-        static unordered_map<string, ScaleType> map = {
-            {"CM", ScaleType::CM}
-        };
-        auto it = map.find(tone); //auto, infiera el tipo de dato (iterador)
-        if (it != map.end())
-            return it->second;
-        return ScaleType::UNKNOWN;
-    }
-    vector<string> scalesDetector(ScaleType scale){
-        switch(scale){
-            case ScaleType::CM:
-            case ScaleType::Am:
-                return {"C","D","E","F","G","A","B"};
-            default:
-                cout << "No existe esa tonalidad" << endl;
-                return {};
-        }
-    }
-};
-
-/*class MyFrame : public wxFrame {
-public:
-    MyFrame() : wxFrame(nullptr, wxID_ANY, "Validador de notas y acordes", wxDefaultPosition, wxSize(600, 250)) {
-        wxPanel* panel = new wxPanel(this);   
-        wxStaticText* text_tonality = new wxStaticText(panel, wxID_ANY, "Tonalidad", wxPoint(50, 50));
-        wxChoice* tone_note = new wxChoice(panel, wxID_ANY, wxPoint(120, 50), wxSize(80,-1));
-        wxChoice* type_scale = new wxChoice(panel, wxID_ANY, wxPoint(200, 50), wxSize(80, -1));
-        wxStaticText* text_sequence = new wxStaticText(panel, wxID_ANY, "Secuencia", wxPoint(50, 80));
-        wxTextCtrl* field_sequence = new wxTextCtrl(panel, wxID_ANY, "", wxPoint(120, 80), wxSize(110, -1));
-        wxButton* validate_sequence = new wxButton(panel, wxID_ANY, "Validar", wxPoint(230, 80), wxSize(50, -1));
-    }
-};*/
 class LeftPanel : public wxPanel{
 public:
     LeftPanel(wxWindow* parent) : wxPanel(parent){
@@ -190,6 +149,154 @@ public:
 class MyApp : public wxApp {
 public:
     bool OnInit() {
+
+        unordered_map<string, vector<string>> scaleMap = {
+            {"CM",  {"C","D","E","F","G","A","B"}},
+            {"GM",  {"G","A","B","C","D","E","F#"}},
+            {"DM",  {"D","E","F#","G","A","B","C#"}},
+            {"AM",  {"A","B","C#","D","E","F#","G#"}},
+            {"EM",  {"E","F#","G#","A","B","C#","D#"}},
+            {"BM",  {"B","C#","D#","E","F#","G#","A#"}},
+            {"F#M", {"F#","G#","A#","B","C#","D#","E#"}},
+            {"C#M", {"C#","D#","E#","F#","G#","A#","B#"}},
+
+            {"FM",  {"F","G","A","Bb","C","D","E"}},
+            {"BbM", {"Bb","C","D","Eb","F","G","A"}},
+            {"EbM", {"Eb","F","G","Ab","Bb","C","D"}},
+            {"AbM", {"Ab","Bb","C","Db","Eb","F","G"}},
+
+            {"Am",  {"A","B","C","D","E","F","G"}},
+            {"Em",  {"E","F#","G","A","B","C","D"}},
+            {"Bm",  {"B","C#","D","E","F#","G","A"}},
+            {"F#m", {"F#","G#","A","B","C#","D","E"}},
+            {"C#m", {"C#","D#","E","F#","G#","A","B"}},
+            {"G#m", {"G#","A#","B","C#","D#","E","F#"}},
+            {"D#m", {"D#","E#","F#","G#","A#","B","C#"}},
+            {"A#m", {"A#","B#","C#","D#","E#","F#","G#"}},
+
+            {"Dm",  {"D","E","F","G","A","Bb","C"}},
+            {"Gm",  {"G","A","Bb","C","D","Eb","F"}},
+            {"Cm",  {"C","D","Eb","F","G","Ab","Bb"}},
+            {"Fm",  {"F","G","Ab","Bb","C","Db","Eb"}}
+        };
+
+        unordered_map<string, vector<string>> chordMap = {
+            {"CM",   {"C","E","G"}},
+            {"Cm",  {"C","Eb","G"}},
+            {"C7",  {"C","E","G","Bb"}},
+            {"Cm7", {"C","Eb","G","Bb"}},
+
+            {"C#M",   {"C#","E#","G#"}},
+            {"C#m",  {"C#","E","G#"}},
+            {"C#7",  {"C#","E#","G#","B"}},
+            {"C#m7", {"C#","E","G#","B"}},
+
+            {"DbM",   {"Db","F","Ab"}},
+            {"Dbm",  {"Db","Fb","Ab"}},
+            {"Db7",  {"Db","F","Ab","Cb"}},
+            {"Dbm7", {"Db","Fb","Ab","Cb"}},
+
+            {"DM",   {"D","F#","A"}},
+            {"Dm",  {"D","F","A"}},
+            {"D7",  {"D","F#","A","C"}},
+            {"Dm7", {"D","F","A","C"}},
+
+            {"D#M",   {"D#","F##","A#"}},
+            {"D#m",  {"D#","F#","A#"}},
+            {"D#7",  {"D#","F##","A#","C#"}},
+            {"D#m7", {"D#","F#","A#","C#"}},
+
+            {"EbM",   {"Eb","G","Bb"}},
+            {"Ebm",  {"Eb","Gb","Bb"}},
+            {"Eb7",  {"Eb","G","Bb","Db"}},
+            {"Ebm7", {"Eb","Gb","Bb","Db"}},
+
+            {"EM",   {"E","G#","B"}},
+            {"Em",  {"E","G","B"}},
+            {"E7",  {"E","G#","B","D"}},
+            {"Em7", {"E","G","B","D"}},
+
+            {"FM",   {"F","A","C"}},
+            {"Fm",  {"F","Ab","C"}},
+            {"F7",  {"F","A","C","Eb"}},
+            {"Fm7", {"F","Ab","C","Eb"}},
+
+            {"F#M",   {"F#","A#","C#"}},
+            {"F#m",  {"F#","A","C#"}},
+            {"F#7",  {"F#","A#","C#","E"}},
+            {"F#m7", {"F#","A","C#","E"}},
+
+            {"GbM",   {"Gb","Bb","Db"}},
+            {"Gbm",  {"Gb","Bbb","Db"}},
+            {"Gb7",  {"Gb","Bb","Db","Fb"}},
+            {"Gbm7", {"Gb","Bbb","Db","Fb"}},
+
+            {"GM",   {"G","B","D"}},
+            {"Gm",  {"G","Bb","D"}},
+            {"G7",  {"G","B","D","F"}},
+            {"Gm7", {"G","Bb","D","F"}},
+
+            {"G#M",   {"G#","B#","D#"}},
+            {"G#m",  {"G#","B","D#"}},
+            {"G#7",  {"G#","B#","D#","F#"}},
+            {"G#m7", {"G#","B","D#","F#"}},
+
+            {"AbM",   {"Ab","C","Eb"}},
+            {"Abm",  {"Ab","Cb","Eb"}},
+            {"Ab7",  {"Ab","C","Eb","Gb"}},
+            {"Abm7", {"Ab","Cb","Eb","Gb"}},
+
+            {"AM",   {"A","C#","E"}},
+            {"Am",  {"A","C","E"}},
+            {"A7",  {"A","C#","E","G"}},
+            {"Am7", {"A","C","E","G"}},
+
+            {"A#M",   {"A#","C##","E#"}},
+            {"A#m",  {"A#","C#","E#"}},
+            {"A#7",  {"A#","C##","E#","G#"}},
+            {"A#m7", {"A#","C#","E#","G#"}},
+
+            {"BbM",   {"Bb","D","F"}},
+            {"Bbm",  {"Bb","Db","F"}},
+            {"Bb7",  {"Bb","D","F","Ab"}},
+            {"Bbm7", {"Bb","Db","F","Ab"}},
+
+            {"BM",   {"B","D#","F#"}},
+            {"Bm",  {"B","D","F#"}},
+            {"B7",  {"B","D#","F#","A"}},
+            {"Bm7", {"B","D","F#","A"}}
+        };
+
+        string tonalidad = "CM";
+        string secuencia = "C E G";
+
+        string note = "";
+        vector<string> incorrectNotes;
+        bool isValid = true;
+        for(char& c : secuencia){
+            if(c == ' '){
+                if(std::find(scaleMap[tonalidad].begin(), scaleMap[tonalidad].end(), note) == scaleMap[tonalidad].end()){
+                    if(chordMap.find(note) != chordMap.end()){
+                        for(string& chordNote : chordMap[note]){
+                            if(std::find(scaleMap[tonalidad].begin(), scaleMap[tonalidad].end(), chordNote) == scaleMap[tonalidad].end()){
+                                incorrectNotes.push_back(note);
+                                isValid = false;
+                            }
+                        }
+                    }
+                    incorrectNotes.push_back(note);
+                    isValid = false;
+                }
+                note = "";
+            }
+            else{
+                note += c;
+            }
+                
+            
+        }
+
+
         MyFrame* frame = new MyFrame();
         frame->Show(true);
         return true;
